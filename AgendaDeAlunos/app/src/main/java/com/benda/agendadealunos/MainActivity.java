aebf119494f49921d4427fd7c26f464a9ddc1b12
Bendazzoli <paulo.bendazzoli@matera.com>
2019-08-31 17:47:38 -0300

    Aula 2.1 Criando o activity_main

--- AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
@@ -2,7 +2,6 @@ package com.benda.agendadealunos;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -11,6 +10,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Primeira Mensagem!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
     }
 }
