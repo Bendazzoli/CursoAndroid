01b329cc2431a68858a3469776cca0fdfc29dbc9
Bendazzoli <paulo.bendazzoli@matera.com>
2019-08-31 17:27:15 -0300

    Aula 1.11 Incluindo primeiro comportamento na Activity

--- AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
@@ -1,8 +1,16 @@
 package com.benda.agendadealunos;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Primeira Mensagem!", Toast.LENGTH_SHORT).show();
+    }
 }
