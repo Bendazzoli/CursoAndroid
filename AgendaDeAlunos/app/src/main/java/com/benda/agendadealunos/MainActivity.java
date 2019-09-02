e12698f9dbcd4655001cfc809e5084e1efad1f55
Bendazzoli <paulo.bendazzoli@matera.com>
2019-09-01 21:52:35 -0300

    Utilizando AdpaterViews para coleção de views

--- AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
@@ -2,8 +2,14 @@ package com.benda.agendadealunos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +17,8 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Huguinho","Zezinho","Luizinho", "Pedrinho", "Claudinho", "Buchecha", "Romário"));
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
