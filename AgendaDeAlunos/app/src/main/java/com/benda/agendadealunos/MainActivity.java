c7a61e719f562e64badff6d2faef73f36b9768a0
Bendazzoli <paulo.bendazzoli@matera.com>
2019-09-05 23:11:01 -0300

    Criando imagem para FloatingActionButton e, usando a AppCompatActivity para setar título manualmente

--- AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
+++ AgendaDeAlunos/app/src/main/java/com/benda/agendadealunos/MainActivity.java
@@ -6,17 +6,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Huguinho","Zezinho","Luizinho", "Pedrinho", "Claudinho", "Buchecha", "Romário"));
         ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         listaAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
