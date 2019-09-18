fca3bf25e015f452465b38d4d11b4921d1a3aa9b
Bendazzoli <paulo.bendazzoli@matera.com>
2019-09-18 10:23:19 -0300

    Adicionando round na conta do preco

--- CalculadoraPrecoCerveja/app/src/main/java/com/benda/calculadoraprecocerveja/model/Cerveja.java
+++ CalculadoraPrecoCerveja/app/src/main/java/com/benda/calculadoraprecocerveja/model/Cerveja.java
@@ -2,6 +2,9 @@ package com.benda.calculadoraprecocerveja.model;
 
 import androidx.annotation.NonNull;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Cerveja {
     private final String nome;
     private final Integer litragem;
@@ -20,6 +23,8 @@ public class Cerveja {
     }
 
     private Float calculaPrecoLitro() {
-        return (this.preco * 1000) / this.litragem;
+        DecimalFormat df = new DecimalFormat("###.##");
+        df.setRoundingMode(RoundingMode.UP);
+        return Float.valueOf(df.format((this.preco * 1000) / this.litragem));
     }
 }
